87e3b02100b56472008db4aefbe9f6c53103d8f9
Thanos Mourikis <athanasios.mourikis@kcl.ac.uk>
2018-05-05 19:57:59 +0200

    extraction of training and prediction set added

---
+++ R/dependencies.R
@@ -0,0 +1,23 @@
+pkgsR  =  c('plyr',
+            'tidyr',
+            'dplyr')
+for (pkgR in pkgsR){
+  if (!pkgR %in% rownames(installed.packages())) {
+    install.packages(pkgR, dependencies=TRUE, INSTALL_opts = c('--no-lock'), repos = "https://cran.ma.imperial.ac.uk/")
+    library(pkgR, character.only = TRUE, quietly = TRUE)
+  } else {
+    library(pkgR, character.only = TRUE, quietly = TRUE)
+  }
+}
+
+source("http://bioconductor.org/biocLite.R")
+
+pkgs  =  c("RDRToolbox")
+for (pkg in pkgs){
+  if (!pkg %in% rownames(installed.packages())) {
+    biocLite(pkg)
+    library(pkg, character.only = TRUE, quietly = TRUE)
+  } else {
+    library(pkg, character.only = TRUE, quietly = TRUE)
+  }
+}
--- R/syssvm.R
+++ R/syssvm.R
@@ -1,18 +1,32 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+#' Run sysSVM
+#'
+#' \code{syssvm} uses e1071 to run an one-class SVM
+#'
+#'
+#' @param object an object, whose class determines
+#'
+#'
+#'
+#'
 
-syssvm <- function() {
-  print("Hello, world!")
+syssvm <- function(input.file=NULL, output.dir=NULL, exclude.features=NULL, models=NULL, scaling.factors=NULL) {
+
+  if(is.null(input.file) | is.null(output.dir)){
+    stop("sysSVM ERROR: Input/Output file unknown, please provide input file and output directory")
+  }
+
+  trainingMode = TRUE
+  if(!is.null(models) & !is.null(scaling.factors)){
+    cat("Training mode disabled, using given models to predict cancer genes...", "\n")
+    trainingMode = FALSE
+  }
+
+  #source("dependecies.R")
+  source("R/prepare.R")
+  source("R/dependencies.R")
+
+  createDescribeTrainingCGC(input.file=input.file, output.dir=output.dir, exclude.features=exclude.features,
+                            trainingMode=trainingMode, models=models, scaling.factors=scaling.factors)
+
+
 }
